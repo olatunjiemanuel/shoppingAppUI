c0bbde96bdf135903ff67c20b892cd5997eaf06b
olatunjiemanuel <olatunjiemanuel15@gmail.com>
2022-07-14 10:15:21 +0100

    updating packages

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -15,7 +15,7 @@ import CategoryComponent from '../components/CatergoryComponent';
 
 const homeScreen = () => {
   return (
-    <View style={{paddingTop: 40, backgroundColor: 'red', flex: 1}}>
+    <View style={styles.homeContainer}>
       <Text>HomeScreen</Text>
     </View>
   );
@@ -26,24 +26,7 @@ export default homeScreen;
 const styles = StyleSheet.create({
   homeContainer: {
     paddingTop: 40,
-    backgroundColor: '#fff',
-  },
-  itemWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  textContainer: {
-    marginTop: 36,
-    alignSelf: 'center',
-    borderWidth: 1,
-    width: 184,
-    height: 35,
-    justifyContent: 'center',
-  },
-  text: {
-    textAlign: 'center',
-    fontWeight: '500',
-    lineHeight: 18.77,
-    fontSize: 16,
+    backgroundColor: 'red',
+    flex: 1,
   },
 });
